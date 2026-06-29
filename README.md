789facbbdef4d3b1daacce2ad8b72eb7eec15e50
srosset81 <srosset81@gmail.com>
2026-06-29 19:58:03 +0200

    Disable telemetry

--- README.md
+++ README.md
@@ -1,3 +1,10 @@
 ## Refine app for NextGraph
 
 A simple app that use Refine providers compatible with NextGraph 
+
+## Quick start
+
+```
+npm install
+npm run dev
+```
--- src/App.tsx
+++ src/App.tsx
@@ -68,6 +68,7 @@ const App: React.FC = () => (
             syncWithLocation: true,
             warnWhenUnsavedChanges: true,
             liveMode: "auto",
+            disableTelemetry: true,
           }}
         >
           <Routes>
