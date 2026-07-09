b30720c6a3c45cf89d05e7601ee9fd75b446a76a
srosset81 <srosset81@gmail.com>
2026-07-09 11:11:29 +0200

    Fix

--- src/providers/utils.ts
+++ src/providers/utils.ts
@@ -2,10 +2,11 @@ import { OrmSubscription } from "@ng-org/orm";
 import type { ShapeType } from "@ng-org/shex-orm";
 import { DeepSignalSet } from "@ng-org/alien-deepsignals"
 
-export const getSignalObject = async (shapeType: ShapeType<any>, ids?: string[]) : Promise<DeepSignalSet<any>> => {
+export const getSignalObject = async (shapeType: ShapeType<any>, ids?: string[], closeSubscription: boolean = true) : Promise<DeepSignalSet<any>> => {
   const subscription = OrmSubscription.getOrCreate(shapeType, { graphs: ids || [''] });
   await subscription.readyPromise;
   const { signalObject } = subscription;
-  // subscription.close();
+  if (closeSubscription) subscription.close();
   return signalObject;
 }
+
